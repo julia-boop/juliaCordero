38d721819b71359e11d9abbb36d4377597ae1f46
julia-boop <crdr.jc@gmail.com>
2021-01-30 19:11:57 -0300

    digital y home listo

--- src/routers/mainRouter.js
+++ src/routers/mainRouter.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 router.get('/', function(req, res){
     res.render('home')
+});
+router.get('/digital', function(req, res){
+    res.render('digital')
 })
 
 module.exports = router
