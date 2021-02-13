bad9cb9e0e6dd7d02b5df3c8599a5b3dc7b6eed3
julia-boop <crdr.jc@gmail.com>
2021-02-12 21:00:01 -0300

    cambios para touch

--- public/js/home.js
+++ public/js/home.js
@@ -2,13 +2,35 @@ window.addEventListener('load', function(){
 
     let btn = document.querySelector('.btnMenu');
     let menuNav = document.querySelector('.sidebar');
+
+    let homeNav = document.querySelector('.homenav');
+    let digital = document.querySelector('.digitalnav');
+    let analogic = document.querySelector('.analogicnav');
+    let contact = document.querySelector('.contactnav');
     
     btn.addEventListener('click', function(){
         btn.classList.toggle('click');
         menuNav.classList.toggle('show');
 
     })
-    
+
+    console.log(homeNav)
+
+    homeNav.addEventListener('touchstart', function(){
+        window.location.href = '/'
+    })
+
+    digital.addEventListener('touchstart', function(){
+        window.location.href = '/digital'
+    })
+
+    analogic.addEventListener('touchstart', function(){
+        window.location.href = '/analogic'
+    })
+
+    contact.addEventListener('touchstart', function(){
+        window.location.href = '/contact'
+    })
 })
 
 
